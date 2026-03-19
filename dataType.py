0859caac02a10e70d390b9ae65b97d9f13bf0cff
davadaviar <davadaviarsaputra2345@gmail.com>
2026-03-19 11:16:20 +0700

    Commit : First commit

---
+++ dataType.py
@@ -0,0 +1,41 @@
+#Integer
+integer_var = 10
+print('Integer:', integer_var)
+
+#Float
+float_var = 3.14
+print('Float:', float_var)
+
+#String
+string_var = 'Hello'
+print('String:', string_var)
+
+#Boolean
+boolean_var = True
+print('Boolean:', boolean_var)
+
+#None
+none_var = None
+print('None:', none_var)
+
+#Dictionary
+dict_var = {'a': 1, 'b': 2, 'c': 3}
+print('Dictionary:', dict_var)
+
+#Set (Data must be unique)
+set_var = {1, 2, 3}
+print('Set:', set_var)
+
+#Tuple (Immutable)
+tuple_var = (1, 2, 3)
+print('Tuple:', tuple_var)
+
+#List (Mutable)
+list_var = [1, 2, 3]
+print('List:', list_var)
+
+#Range
+range_var = range(1, 10)
+print('Range:', range_var)
+
+
